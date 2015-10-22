57e76f3aa6fbb4ce1d5c9ef5aaf74543be3b763b
jzorrof@gmail.com <jzorrof@gmail.com>
2015-10-22 14:47:41 +0800

    update

--- settings.py
+++ settings.py
@@ -1,11 +1,1 @@
 DOMAIN = {'people': {}}
-
-# Let's just use the local mongod instance. Edit as needed.
-
-# Please note that MONGO_HOST and MONGO_PORT could very well be left
-# out as they already default to a bare bones local 'mongod' instance.
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
-MONGO_USERNAME = 'user'
-MONGO_PASSWORD = 'user'
-MONGO_DBNAME = 'apitest'
