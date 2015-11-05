c2787e0d0a0349fa8f2d7630b02c7871bf755578
jzorrof@gmail.com <jzorrof@gmail.com>
2015-11-05 21:15:36 +0800

    revert

--- run.py
+++ run.py
@@ -3,11 +3,13 @@
 from eve import Eve
 from eve.auth import BasicAuth
 
+app = Eve(auth=MyBasicAuth)
+
+
 class MyBasicAuth(BasicAuth):
     def check_auth(self, username, password, allowed_roles, resource,
                    method):
         return username == 'admin' and password == 'secret'
 
-# if __name__ == '__main__':
-app = Eve(auth=MyBasicAuth)
-app.run()
+if __name__ == '__main__':
+	app.run()
