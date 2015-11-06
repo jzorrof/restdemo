b1c46900af385f678196daaf7103681fff6a0508
jzorrof@gmail.com <jzorrof@gmail.com>
2015-11-06 13:37:03 +0800

    update

--- bcrypt.py
+++ bcrypt.py
@@ -5,8 +5,14 @@ import bcrypt
 from eve.auth import BasicAuth
 
 
-class BCryptAuth(object):
+class BCryptAuth(BasicAuth):
     """docstring for BCryptAuth"""
-    def check_auth(self, username, password, allowed_roles, resource, method):
+    def check_auth(self, username, password, allowed_roles, resource,
+                   method):
         if resource == 'accounts':
             return username == 'superuser' and password == 'password'
+        else:
+            accounts = app.data.drive.db['accounts']
+            account = accounts.find_one({'username': username})
+            return account and \
+                   bcrypt.hashpwd(password, account['password']) == account['password']
--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from eve.auth import BasicAuth
 from eve import Eve
-class MyBasicAuth(BasicAuth):
-    def check_auth(self, username, password, allowed_roles, resource,
-                   method):
-        return username == 'admin' and password == 'secret'
 
 app = Eve(auth=MyBasicAuth)
 if __name__ == '__main__':
