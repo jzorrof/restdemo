3274b63dd2ad24260515baefaf9abd79d6eec795
jzorrof@gmail.com <jzorrof@gmail.com>
2015-11-06 11:34:14 +0800

    update

--- bcrypt.py
+++ bcrypt.py
@@ -7,8 +7,7 @@ from eve.auth import BasicAuth
 
 class BCryptAuth(object):
     """docstring for BCryptAuth"""
-
-    def check_auth(self, username, password, allowed_roles, methods):
+    def check_auth(self, username, password, allowed_roles, resource, method):
         if resource == 'accounts':
             return username == 'superuser' and password == 'password'
         else:
